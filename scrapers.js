c1f21a2042e92c72cc17e641b086d29aa0487a1b
Manuel <m_guzman74@yahoo.com>
2022-01-17 01:08:03 -0600

    added sleep function between split loops to reduce ram and cpu usage

--- scrapers.js
+++ scrapers.js
@@ -20,8 +20,6 @@ const mailOptions = {
 };
 
 async function BBScrape(url) {
-  console.time("loop time");
-
   const browser = await puppeteer.launch({
     args: [
       "--autoplay-policy=user-gesture-required",
@@ -113,39 +111,46 @@ async function BBScrape(url) {
     await browser.close();
   } finally {
     await browser.close();
-    console.timeEnd("loop time");
   }
 }
 
-const scrapeAddresses = {
-  BB: [
-    "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-aorus-master-10gb-rev-2-0-gddr6x-pci-express-4-0-graphics-card/6462198.p?skuId=6462198",
-    "https://www.bestbuy.com/site/asus-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6432445.p?skuId=6432445",
-    "https://www.bestbuy.com/site/evga-rtx-3080-12gb-ftw3-ultra-gaming-12g-p5-4877-kl-pci-express-4-0-lhr/6494860.p?skuId=6494860",
-    "https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-ventus-3x-10g-oc-bv-gddr6x-pci-express-4-0-graphic-card-black-sliver/6430175.p?skuId=6430175",
-    "https://www.bestbuy.com/site/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6432400.p?skuId=6432400",
-    "https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-ventus-3x-10g-oc-lhr-gddr6x-pci-express-4-0-graphic-card-black/6471287.p?skuId=6471287",
-    "https://www.bestbuy.com/site/asus-geforce-rtx-3080-v2-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6475238.p?skuId=6475238",
-    "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-aorus-master-10gb-rev3-0-gddr6x-pci-express-4-0-graphics-card/6471950.p?skuId=6471950",
-    "https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-gaming-z-trio-10g-lhr-10gb-gddr6x-pci-express-4-0-graphic-card-black/6480289.p?skuId=6480289",
-    "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-vision-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6471957.p?skuId=6471957",
-    "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-vision-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6436219.p?skuId=6436219",
-    "https://www.bestbuy.com/site/evga-rtx-3080-xc3-ultra-gaming-10g-p5-3885-kh-pci-express-4-0-lhr/6471615.p?skuId=6471615",
-    "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440",
-  ],
-};
+const scrapeAddresses = [
+  "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-aorus-master-10gb-rev-2-0-gddr6x-pci-express-4-0-graphics-card/6462198.p?skuId=6462198",
+  "https://www.bestbuy.com/site/asus-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6432445.p?skuId=6432445",
+  "https://www.bestbuy.com/site/evga-rtx-3080-12gb-ftw3-ultra-gaming-12g-p5-4877-kl-pci-express-4-0-lhr/6494860.p?skuId=6494860",
+  "https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-ventus-3x-10g-oc-bv-gddr6x-pci-express-4-0-graphic-card-black-sliver/6430175.p?skuId=6430175",
+  "https://www.bestbuy.com/site/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6432400.p?skuId=6432400",
+  "https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-ventus-3x-10g-oc-lhr-gddr6x-pci-express-4-0-graphic-card-black/6471287.p?skuId=6471287",
+  "https://www.bestbuy.com/site/asus-geforce-rtx-3080-v2-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6475238.p?skuId=6475238",
+  "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-aorus-master-10gb-rev3-0-gddr6x-pci-express-4-0-graphics-card/6471950.p?skuId=6471950",
+  "https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-gaming-z-trio-10g-lhr-10gb-gddr6x-pci-express-4-0-graphic-card-black/6480289.p?skuId=6480289",
+  "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-vision-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6471957.p?skuId=6471957",
+  "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-vision-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6436219.p?skuId=6436219",
+  "https://www.bestbuy.com/site/evga-rtx-3080-xc3-ultra-gaming-10g-p5-3885-kh-pci-express-4-0-lhr/6471615.p?skuId=6471615",
+  "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440",
+];
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const intervalFunc = async () => {
-  // for (let i = 0; i <= addressArray.length - 1; i++) {
-  //   scraper(addressArray[i]);
-  // }
-  for (const address of scrapeAddresses.BB) {
-    await BBScrape(address);
+  const half = Math.ceil(scrapeAddresses.length / 2);
+  const firstHalf = scrapeAddresses.slice(0, half);
+  const secondHalf = scrapeAddresses.slice(-half);
+
+  for (let i = 0; i <= firstHalf.length - 1; i++) {
+    BBScrape(firstHalf[i]);
+  }
+  //sleep function alleviates ram and cpu usage from too many synchronous scrapes
+  await sleep(10000);
+
+  for (let i = 0; i <= secondHalf.length - 1; i++) {
+    BBScrape(secondHalf[i]);
   }
 };
-intervalFunc();
-// setInterval(() => {
-//   intervalFunc(scrapeAddresses.BB, BBScrape);
-// }, 5000);
+// intervalFunc();
 
-// BBScrape(scrapeAddresses.BB[0]);
+setInterval(() => {
+  intervalFunc();
+}, 30000);
